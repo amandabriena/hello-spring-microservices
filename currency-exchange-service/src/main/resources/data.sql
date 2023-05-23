4d5b84f0c200cfe09ada905b56437cf228d3613c
amandabriena <amanda.briena@gmail.com>
2023-05-22 22:20:25 -0300

    Currency Conversion Microservice
     - Added dependencies for Data JPA and h2 database
    - Made CurrencyExchange an entity
    - Populate data in data.sql

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1001, 'USD', 'BRL',65,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1002, 'BRL', 'USD',75,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1003, 'EUR', 'BRL',100,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1004, 'USD', 'EUR',100,'');
