cb0ccf9f70cb58e1adbe653c8c17268a6a7d4dfc
amandabriena <amanda.briena@gmail.com>
2023-05-22 23:31:02 -0300

    Create Currency Conversion Service
     - Create Currency Conversion Service integrated to Currency Exchange service

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,12 +1,18 @@
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(1001, 'USD', 'BRL',65,'');
+values(1001, 'USD', 'BRL',4.97,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(1002, 'BRL', 'USD',75,'');
+values(1002, 'BRL', 'USD',0.2,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(1003, 'EUR', 'BRL',100,'');
+values(1003, 'EUR', 'BRL',5.37,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(1004, 'USD', 'EUR',100,'');
+values(1004, 'BRL', 'EUR',0.19,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1005, 'USD', 'EUR',0.93,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1006, 'EUR', 'USD',1.08,'');
