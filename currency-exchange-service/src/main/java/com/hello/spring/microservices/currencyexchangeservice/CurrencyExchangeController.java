acab15fa943b1fbf6c1bfa70e39935c2365a4cc8
amandabriena <amanda.briena@gmail.com>
2023-06-13 23:26:42 -0300

    Configure docker and distributed trace
     - Start to configure docker container and zipkin distributed trace

--- currency-exchange-service/src/main/java/com/hello/spring/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/hello/spring/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.hello.spring.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-	
+
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	@Autowired
 	private CurrencyExchangeRepository repository;
 	
@@ -20,7 +23,7 @@ public class CurrencyExchangeController {
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		//CurrencyExchange currencyExchange = new CurrencyExchange(10000L, from, to, BigDecimal.valueOf(50));
-		
+		logger.info("retruieveExchangeValue called with {} to {}", from, to);
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		
 		if(currencyExchange == null) {
